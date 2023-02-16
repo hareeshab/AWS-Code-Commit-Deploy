855048d4dea99ae6f92b9e113e30bda0acd4e9c1
Hareesha <32353896+hareeshab@users.noreply.github.com>
2023-02-16 08:46:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo NodeJS</h1> <h4>Message: Successfully deploy using AWS code deploy and code pipeline</h4> <p>Version 2.0</p>');
+  res.send('<h1>Express Demo NodeJS</h1> <h4>Message: Successfully deploy using AWS code deploy and code pipeline</h4> <p>Version 3.0</p>');
 })
 
 app.get('/products', (req, res) => {
   res.send([
     {
       productId: '101',
-      price: 150
+      price: 200
     },
     {
       productId: '102',
-      price: 250
+      price: 300
     }
   ])
 })
