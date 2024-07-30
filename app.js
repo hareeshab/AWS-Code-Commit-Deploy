2fa4653a52e3c6461ea9812ca1a9cb7721cac143
Hareesha <32353896+hareeshab@users.noreply.github.com>
2024-07-30 08:12:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo NodeJS</h1> <h4>Message: Successfully deploy using AWS Ec2-code deploy and code pipeline</h4> <p>Version 1.0</p>');
+  res.send('<h1>Express Demo NodeJS</h1> <h4>Message: Successfully deploy using Manual deployment</h4> <p>Version 1.0</p>');
 })
 
 app.get('/products', (req, res) => {
